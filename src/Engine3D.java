ced44914f8adc2bd972a828bba9a33f024a3caa7
Baptiste <martin.baptist3@gmail.com>
2025-11-18 19:44:38 +0100

    9eme commit developp fin de reconstruction mais MARCHE et teaPot marche + aspect ratio dynamique avec les modifs de taille de la fenetre

--- src/Engine3D.java
+++ src/Engine3D.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Engine3D extends JPanel {
-    private Mesh meshCube;
-    private int width;
-    private int height;
+    private Mesh mesh;
+    private int prevWidth;
+    private int prevHeight;
     private double theta;
     private Vertex3D vCamera;
     private Timer elapsedTime;
 
-    public Engine3D(int width, int height) {
-        this.width = width;
-        this.height = height;
+    public Engine3D(int widthInit, int heightInit) {
+        this.prevWidth = widthInit;
+        this.prevHeight = heightInit;
         this.theta = 0;
         this.vCamera = new Vertex3D();
 
-        this.meshCube = new Mesh();
+        this.mesh = new Mesh();
         setBackground(Color.BLACK);
 
         this.elapsedTime = new Timer(16, e -> repaint());
@@ -26,20 +26,18 @@ public class Engine3D extends JPanel {
 
 
         //.OBJ file reading + construction of the 3D triangle to render
-        //Document.readObjFile(Paths.get("C:\\Users\\marti\\Desktop\\premier test.obj"),this.meshCube);
-
-        Document.readObjFile(Paths.get("C:\\Users\\marti\\Desktop\\test2.obj"),this.meshCube);
-        this.meshCube.triConstruct();
+        Document.readObjFile(Paths.get("C:\\Users\\marti\\Desktop\\obj model\\teapot.obj"),this.mesh);
+        this.mesh.triConstruct();
 
         //Projection matrix coefficient value(a require)
         double fNear = 0.1;
         double fFar = 1000;
         double fFov = 90;
 
-        //Projection matrix coefficient definition
-        this.meshCube.setMatProj(Matrix.matrixCreateProjection4x4(fNear,fFar,fFov,height,width));
+        //Projection matrix coefficient definition initialisation
+        this.mesh.setMatProj(Matrix.matrixCreateProjection4x4(fNear,fFar,fFov,heightInit,widthInit));
 
-        printMatrix(this.meshCube.getMatProj());
+        printMatrix(this.mesh.getMatProj());
     }
 
     public void printMatrix(Matrix m) {
@@ -52,43 +50,46 @@ public class Engine3D extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        int currentWidth = getWidth();
+        int currentHeight = getHeight();
 
+        if (currentWidth != prevWidth || currentHeight != prevHeight) {
+            this.prevWidth = currentWidth;
+            this.prevHeight = currentHeight;
+
+            double fNear = 0.1;
+            double fFar = 1000;
+            double fFov = 90;
+
+            mesh.setMatProj(Matrix.matrixCreateProjection4x4(fNear, fFar, fFov, prevHeight, prevWidth));
+
+            prevWidth = currentWidth;
+            prevHeight = currentHeight;
+        }
+
+        //Actualisation of theta
         this.theta += 0.05;
         System.out.println(this.theta);
 
         //Rotation matrices Z-axis
         Matrix matRotZ = Matrix.matrixCreateRotationZ4x4(this.theta);
-        System.out.println("Z");
-        printMatrix(matRotZ);
+
         //Rotation matrices X-axis
         Matrix matRotX = Matrix.matrixCreateRotationX4x4(this.theta * 0.5);
-        System.out.println("X");
-        printMatrix(matRotX);
+
         //Z-Axis Offset
         Matrix matTrans = Matrix.matrixMultiplication(matRotZ, matRotX);
-        System.out.println("ZX");
-        printMatrix(matTrans);
         Matrix matWorld = Matrix.matrixMultiplication(matTrans,Matrix.matrixCreateTranslation4x4(0,0,8.5));
 
         List<Triangle> trisToRaster = new ArrayList<Triangle>();
         //Triangle projection and drawing
-        for (Triangle triangleToProject : meshCube.getTris()) {
-
+        for (Triangle triangleToProject : mesh.getTris()) {
             Triangle triTransformed = new Triangle();
 
             //Z-axis and X-axis Rotation
             triangleToProject.getVertices()[0].vertexMatrixMultiplication(triTransformed.getVertices()[0],matWorld);
             triangleToProject.getVertices()[1].vertexMatrixMultiplication(triTransformed.getVertices()[1],matWorld);
             triangleToProject.getVertices()[2].vertexMatrixMultiplication(triTransformed.getVertices()[2],matWorld);
-
-//            //Z-axis Offset
-//            Triangle triTranslated;
-//            triTranslated = triangleToProject;
-//
-//            triTransformed.getVertices()[0].setZ(triTranslated.getVertices()[0].getZ() + 8.5);
-//            triTransformed.getVertices()[1].setZ(triTranslated.getVertices()[1].getZ() + 8.5);
-//            triTransformed.getVertices()[2].setZ(triTranslated.getVertices()[2].getZ() + 8.5);
-
 
             //line creation for determining the normal
             Vertex3D line1 = Vertex3D.vertexSubtraction(triTransformed.getVertices()[1],triTransformed.getVertices()[0]);
@@ -113,11 +114,12 @@ public class Engine3D extends JPanel {
                 triTransformed.setColor(colorTri); //Definition of the greyscale value for the triangle regarding its orienttion
 
                 //Projecting 3D into 2D
-                triTransformed.getVertices()[0].vertexMatrixMultiplication(triProjected.getVertices()[0], this.meshCube.getMatProj());
-                triTransformed.getVertices()[1].vertexMatrixMultiplication(triProjected.getVertices()[1], this.meshCube.getMatProj());
-                triTransformed.getVertices()[2].vertexMatrixMultiplication(triProjected.getVertices()[2], this.meshCube.getMatProj());
+                triTransformed.getVertices()[0].vertexMatrixMultiplication(triProjected.getVertices()[0], this.mesh.getMatProj());
+                triTransformed.getVertices()[1].vertexMatrixMultiplication(triProjected.getVertices()[1], this.mesh.getMatProj());
+                triTransformed.getVertices()[2].vertexMatrixMultiplication(triProjected.getVertices()[2], this.mesh.getMatProj());
                 triProjected.setColor(triTransformed.getColor()); //Color transfer
 
+                //Normalization of the vertex
                 triProjected.getVertices()[0].vertexDivision(triProjected.getVertices()[0].getW());
                 triProjected.getVertices()[1].vertexDivision(triProjected.getVertices()[1].getW());
                 triProjected.getVertices()[2].vertexDivision(triProjected.getVertices()[2].getW());
@@ -149,23 +151,22 @@ public class Engine3D extends JPanel {
             double meanZ1 = (t1.getVertices()[0].getZ() + t1.getVertices()[1].getZ() + t1.getVertices()[2].getZ()) / 3;
             double meanZ2 = (t2.getVertices()[0].getZ() + t2.getVertices()[1].getZ() + t2.getVertices()[2].getZ()) / 3;
             return Double.compare(meanZ2,meanZ1);
-        });
+        }); //Painter's algorithm
 
         for (Triangle triBeingDrawn : trisToRaster) {
-            Vertex3D[] v = triBeingDrawn.getVertices();
 
             int[] xs = new int[3];
             int[] ys = new int[3];
 
             for (int i = 0; i < 3; i++) {
-                xs[i] = (int) Math.round(v[i].getX());
-                ys[i] = (int) Math.round(v[i].getY());
+                xs[i] = (int) Math.round(triBeingDrawn.getVertices()[i].getX());
+                ys[i] = (int) Math.round(triBeingDrawn.getVertices()[i].getY());
             } //Getting back the coordinate to draw the 2D triangle
 
             g.setColor(triBeingDrawn.getColor()); //Setting the correct color
 
             Graphics2D g2 = (Graphics2D) g;
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
             g2.fillPolygon(xs, ys, 3); //Rasterization pas encore faites
         }
 
@@ -175,8 +176,8 @@ public class Engine3D extends JPanel {
         return elapsedTime;
     }
 
-    public Mesh getMeshCube() {
-        return meshCube;
+    public Mesh getMesh() {
+        return mesh;
     }
 
     public static void main(String[] args) {
@@ -187,8 +188,8 @@ public class Engine3D extends JPanel {
         window.setSize(width, height);
 
         Engine3D engine3D = new Engine3D(width, height);
-        window.add(engine3D);              // d'abord ajouter le panel
-        window.setLocationRelativeTo(null); // centrer la fenêtre
-        window.setVisible(true);// puis afficher
+        window.add(engine3D);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
